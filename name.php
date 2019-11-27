74240703ca98410f6547bed99fe867cf691cfde7
Mo <moh19951@outlook.com>
2019-11-27 12:12:45 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,12 @@
+<?php
+echo "ik ga twee getalen met elkaar vermedigvuldigen ";
+echo "". PHP_EOL;
+echo "eerste getal?";
+$a = readline(">");
+echo $a, "\n";
+echo "Tweede getal?";
+$b = readline('>');
+echo $b, "\n";
+echo "het antwoord is =";
+echo $a * $b;
+?>
